14b9e80d859aed5e4b6cff4062ad1cf04c7f7a76
Gmanuk101 <graemephillipsuk@gmail.com>
2018-07-16 19:02:50 +0100

    added new tests on a simple html form

--- src/test/java/com/seleniumSimplified/WebDriver/Manipulation/SimpleFormChecking.java
+++ src/test/java/com/seleniumSimplified/WebDriver/Manipulation/SimpleFormChecking.java
@@ -8,14 +8,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.lang.reflect.Array;
 import java.util.List;
 
-import static org.hamcrest.core.Is.is;
+
 
 public class SimpleFormChecking {
 
